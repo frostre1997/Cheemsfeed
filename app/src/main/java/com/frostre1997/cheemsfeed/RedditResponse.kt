d3beee30131c8777ffb35ba5b0b61f6733b95ebb
Frostre_ <n9043395@gmail.com>
2026-07-03 15:43:44 +0200

    Refactor RedditResponse data structure
    Refactor RedditResponse data classes to include RedditChild and PostData.

--- app/src/main/java/com/frostre1997/cheemsfeed/RedditResponse.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RedditResponse.kt
@@ -1,5 +1,24 @@
 package com.frostre1997.cheemsfeed
 
-data class RedditResponse(val data: RedditData)
-data class RedditData(val children: List<RedditChildren>)
-data class RedditChildren(val data: RedditPost)
+data class RedditResponse(
+    val data: RedditData
+)
+
+data class RedditData(
+    val children: List<RedditChild>
+)
+
+data class RedditChild(
+    val data: PostData
+)
+
+data class PostData(
+    val title: String,
+    val author: String?,
+    val url: String?,
+    val thumbnail: String?,
+    val created_utc: Long?,
+    val num_comments: Int?,
+    val score: Int?
+    // add any other fields you need
+)
