1908d6dab02efc2a56d94e52dd87c26b0a2720fd
Frostre_ <n9043395@gmail.com>
2026-07-03 15:44:47 +0200

    Refactor RetrofitClient to use RedditApi interface

--- app/src/main/java/com/frostre1997/cheemsfeed/RetrofitClient.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RetrofitClient.kt
@@ -2,16 +2,22 @@ package com.frostre1997.cheemsfeed
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface RedditApi {
+    @GET("r/cheemsburbger/hot.json")  // change subreddit if needed
+    suspend fun getHotPosts(): RedditResponse
+    // or use Call<RedditResponse> if you prefer; but suspend is cleaner
+}
 
 object RetrofitClient {
     private const val BASE_URL = "https://www.reddit.com/"
 
-    val instance: RedditApiService by lazy {
-        val retrofit = Retrofit.Builder()
+    val instance: RedditApi by lazy {
+        Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
-        retrofit.create(RedditApiService::class.java)
+            .create(RedditApi::class.java)
     }
 }
