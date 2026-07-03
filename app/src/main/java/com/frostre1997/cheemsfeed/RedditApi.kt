dfcdbe913114cf11a05f15f94aafb99ba9668547
Frostre_ <n9043395@gmail.com>
2026-07-03 16:55:24 +0200

    Fix RedditApi: remove duplicate data classes and use Call instead of suspend function

--- app/src/main/java/com/frostre1997/cheemsfeed/RedditApi.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RedditApi.kt
@@ -1,33 +1,14 @@
 package com.frostre1997.cheemsfeed
 
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RedditApi {
     @GET("r/{subreddit}/hot.json")
-    suspend fun getHotPosts(
+    fun getHotPosts(
+        @Path("subreddit") subreddit: String = "Doge",
         @Query("limit") limit: Int = 25
-    ): RedditResponse
-    
+    ): Call<RedditResponse>
 }
-
-data class RedditResponse(
-    val data: RedditData
-)
-
-data class RedditData(
-    val children: List<RedditPost>
-)
-
-data class RedditPost(
-    val data: PostData
-)
-
-data class PostData(
-    val title: String,
-    val selftext: String,
-    val url: String,
-    val author: String,
-    val created_utc: Long,
-    val ups: Int
-)
