435a4a3fcb7a64e80fd9ecce8abe046a9ecb9775
Frostre_ <n9043395@gmail.com>
2026-07-03 18:04:27 +0200

    Fix import statement for MaterialToolbar

--- app/src/main/java/com/frostre1997/cheemsfeed/MainActivity.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.appbars.MaterialToolbar
+import com.google.android.material.appbar.MaterialToolbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
